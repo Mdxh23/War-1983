70c6b45b5015d7bc30440536f27ea6b4bce7ebf0
Maddi Ashwood <hg5441@wayne.edu>
2023-11-09 14:54:35 -0500

    template

--- War 1983/main.swift
+++ War 1983/main.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 //print("Hello, World!")
-print("hey girl")
+
+// Define your game logic here
+func playGame() {
+    let plays = ["Play 1", "Play 2", "Play 3"] // Replace with your actual game plays
+    for play in plays {
+        print(play)
+    }
+}
+
+// Entry point of the program
+func main() {
+    playGame()
+}
+
+// Run the main function
+main()
+
 
